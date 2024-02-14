1ee4bbf1b8d2e50e5ad098d72ff9fd4c848d67b7
hello-world-OHNO <daruma0248@gmail.com>
2024-02-14 14:56:08 +0900

    計算機

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { ChakraProvider } from "@chakra-ui/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,70 +1,90 @@
 "use client";
-import { Box, ChakraProvider } from "@chakra-ui/react";
+import { Box, ChakraProvider, Grid, GridItem, Spacer } from "@chakra-ui/react";
 import NumberBtn from "./components/NumberBtn/index";
 import OperatorBtn from "./components/OperatorBtn/index";
 import EqualBtn from "./components/EqualBtn/index";
-import { use, useState } from 'react';
-import { set, useForm } from "react-hook-form";
+import { useState } from 'react';
 
 export default function Home() {
-  const [display, setDisplay] = useState<number>(0);
+  const [display, setDisplay] = useState<string>("0");
   const [memory, setMemory] = useState<number | null>(null);
   const [operator, setOperator] = useState<string | null>(null);
 
+  // 数字押された時のアクション
   const handleNumberClick = (number: number) => {
-    if (display === 0) {
-      setDisplay(number);
-    } else {
-      setDisplay(prevDisplay => prevDisplay * 10 + number)
+    if (display === "0") {
+      setDisplay(number.toString());
+      // 12桁以下か判定
+    } else if (display.length < 12) {
+      setDisplay(prevDisplay => prevDisplay + number.toString());
     }
   };
 
+  // 四則演算が押された時のアクション
   const handleOperatorClick = (operator: string) => {
-    setMemory(display);
+    setMemory(parseFloat(display));
     setOperator(operator);
-    setDisplay(0);
+    setDisplay("0");
   };
 
+  // ＝が押されたときのアクション
   const handleEqualClick = () => {
     if (memory !== null && operator !== null) {
+      let result: number;
       switch (operator) {
         case '+':
-          setDisplay(memory + display);
+          result = memory + parseFloat(display);
           break;
         case '-':
-          setDisplay(memory - display);
+          result = memory - parseFloat(display);
           break;
         case '*':
-          setDisplay(memory * display);
+          result = memory * parseFloat(display);
           break;
         case '/':
-          setDisplay(memory / display);
+          result = memory / parseFloat(display);
           break;
+        default:
+          result = 0;
       }
+      // 表示結果を12桁を上限にする
+      const resultString = result.toFixed(12);
+      setDisplay(resultString);
       setMemory(null);
       setOperator(null);
     }
   };
-
   return (
     <ChakraProvider>
       <Box border="solid" maxWidth="300px" height="450px" margin="0 auto">
-        <div>{display}</div>
-        <NumberBtn number={1} onClick={handleNumberClick} />
-        <NumberBtn number={2} onClick={handleNumberClick} />
-        <NumberBtn number={3} onClick={handleNumberClick} />
-        <NumberBtn number={4} onClick={handleNumberClick} />
-        <NumberBtn number={5} onClick={handleNumberClick} />
-        <NumberBtn number={6} onClick={handleNumberClick} />
-        <NumberBtn number={7} onClick={handleNumberClick} />
-        <NumberBtn number={8} onClick={handleNumberClick} />
-        <NumberBtn number={9} onClick={handleNumberClick} />
-        <NumberBtn number={0} onClick={handleNumberClick} />
-        <OperatorBtn operator="+" onClick={handleOperatorClick} />
-        <OperatorBtn operator="-" onClick={handleOperatorClick} />
-        <OperatorBtn operator="*" onClick={handleOperatorClick} />
-        <OperatorBtn operator="/" onClick={handleOperatorClick} />
-        <EqualBtn onClick={handleEqualClick} />
+        <Grid
+          templateColumns="repeat(4, 1fr)"
+          gap={2}
+          height="100%"
+          alignItems="center"
+          justifyContent="center"
+          padding={4}
+        >
+          <GridItem colSpan={4} textAlign="right">
+            {display}
+          </GridItem>
+          <NumberBtn number={7} onClick={handleNumberClick} />
+          <NumberBtn number={8} onClick={handleNumberClick} />
+          <NumberBtn number={9} onClick={handleNumberClick} />
+          <OperatorBtn operator="+" onClick={handleOperatorClick} />
+          <NumberBtn number={4} onClick={handleNumberClick} />
+          <NumberBtn number={5} onClick={handleNumberClick} />
+          <NumberBtn number={6} onClick={handleNumberClick} />
+          <OperatorBtn operator="-" onClick={handleOperatorClick} />
+          <NumberBtn number={1} onClick={handleNumberClick} />
+          <NumberBtn number={2} onClick={handleNumberClick} />
+          <NumberBtn number={3} onClick={handleNumberClick} />
+          <OperatorBtn operator="*" onClick={handleOperatorClick} />
+          <NumberBtn number={0} onClick={handleNumberClick} />
+          <Spacer />
+          <EqualBtn onClick={handleEqualClick} />
+          <OperatorBtn operator="/" onClick={handleOperatorClick} />
+        </Grid>
       </Box>
     </ChakraProvider>
   );
